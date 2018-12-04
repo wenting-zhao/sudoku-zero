a3867a879e709b0ce5f18837b01b9d4d953e0c1a
bywbilly <bywbilly@gmail.com>
2018-12-03 20:32:14 -0500

    minor fix

--- train_sl.sh
+++ train_sl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -J sudoku_policy# Job name
-#SBATCH -o sudoku_policy.o%j    # Name of stdout output file (%j expands to jobId)
-#SBATCH -e sudoku_policys%j    # Name of stderr output file
+#SBATCH -J sudoku_pv# Job name
+#SBATCH -o sudoku_pvo%j    # Name of stdout output file (%j expands to jobId)
+#SBATCH -e sudoku_pvv%j    # Name of stderr output file
 #SBATCH -N 1   # Total number of CPU nodes requested
 #SBATCH -n 1   # Total number of CPU cores requrested
 #SBATCH -t 48:00:00    # Run time (hh:mm:ss)
-#SBATCH -p aida --gres=gpu:4 -c 24  # Which queue to run on, and what resources to use
+#SBATCH -p aida --gres=gpu:2 -c 24  # Which queue to run on, and what resources to use
 
 
-python3 -u sudoku_sl.py --log policy --type sudoku_model --model_type 1 --model_path ./modelsave_policy --gpu_list 0,1,2,3 --train_gpu 0,1,2,3 | tee master_policy.log
+python3 -u sudoku_sl.py --log pv --type sudoku_model --model_type 3 --model_path ./modelsave_pv --gpu_list 0,1 --train_gpu 0,1 | tee master_pv.log
 
